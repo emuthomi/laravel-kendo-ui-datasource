0638dce180c21d2e440fd052d98e24b476eb9a69
Rogan <rogan@alivemobile.com>
2013-09-20 00:43:31 +1000

    fucking motherfucker merge conflicts fuck shit fuck

--- src/Meowcakes/LaravelKendoUiDatasource/DataSource.php
+++ src/Meowcakes/LaravelKendoUiDatasource/DataSource.php
@@ -77,7 +77,7 @@ class DataSource
 			else if($d['operator'] === 'endswith')
 				$value = "%$value";
 
-			$query->where($d['field'], $this->stringOps[$d['operator']], $value, $logic);
+			$query->where($d['field'], $d['operator'], $value, $logic);
 		}
 		else if($this->columns[$d['field']] === 'number')
 		{
@@ -87,7 +87,6 @@ class DataSource
 			if( ! isset($d['value']) or ! is_numeric($d['value']))
 				$this->app->abort(400);
 
-<<<<<<< HEAD
 			$query->where($d['field'], $d['value'] === 'true' ? '!=' : '=', 0, $logic);
 		}
 		else if($this->columns[$d['field']] === 'boolean')
@@ -99,9 +98,6 @@ class DataSource
 				$this->app->abort(400);
 
 			$query->where($d['field'], $d['operator'], $d['value'] === 'true' ? 1 : 0, $logic);			
-=======
-			$query->where($d['field'], $this->numberOps[$d['operator']], $d['value'], $logic);
->>>>>>> 30d1343b67b6e2666e824495b7b70335a36d3dd4
 		}
 		else if($this->columns[$d['field']] === 'date')
 		{
@@ -116,7 +112,7 @@ class DataSource
 				$this->app->abort(400);
 			}
 
-			$query->where($d['field'], $this->numberOps[$d['operator']], $value, $logic);
+			$query->where($d['field'], $d['operator'], $value, $logic);
 		}
 		else {
 			$this->app->abort(500);
@@ -154,21 +150,11 @@ class DataSource
 
 	public function execute($query)
 	{
-		if(isset($this->input[$this->sortKey]) and is_array($this->input[$this->sortKey]))
+		if(isset($this->input[$this->sortKey]))
 			$this->sort($query, $this->input[$this->sortKey]);
 
-		if(isset($this->input[$this->filterKey]) and is_array($this->input[$this->filterKey]))
+		if(isset($this->input[$this->filterKey]))
 			$this->filter($query, $this->input[$this->filterKey]);
-
-		$total = $query->count();
-
-		if(isset($this->input['skip']))
-			$query->skip(@intval($this->input['skip']));
-
-		if(isset($this->input['take']))
-			$query->take(@intval($this->input['take']));
-
-		return $total;
 	}
 
 }
